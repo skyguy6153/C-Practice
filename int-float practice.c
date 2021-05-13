34c85de148a44750a304ccef454529f2220809fb
skyguy6153 <53711550+skyguy6153@users.noreply.github.com>
2021-05-12 20:45:41 -0400

    Add files via upload

---
+++ int-float practice.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+printf("Numbers are divided into two groups in coding, integers and floats. Integers are whole numbers with no fractional part or decimal point.\n");
+printf("Floats are number that have a fractional part or decimal point.\n");
+printf("TO change the amount of digits that come before or after a decimal, you'll put the number of the amount you want in front, a decimal point, and the number of the amount you want behind.\n");
+printf("This number will go between the percent and f.\n");
+printf("%d is an integer.\n",27);
+printf("%d is an integer.\n",11);
+printf("%4.1f is a float.\n",1003.2);
+printf("%2.1f is also a float.\n",96.1);
+printf("%3.1f can also be a float.\n",102.3);
+printf("%3.1f is a float.\n",107.9);
+printf("Did you notice that %d is an integer?\n",6);
+printf("%d is a integer.\n",60);
+printf("Don't forget that %d is an integer!\n",99);
+printf("%d equals integer.\n",7);
+printf("Even though %d comes after ten, it's still an integer.\n",11);
+printf("%d is easily an integer...\n",420);
+printf("%d is an integer....nice.\n",69);
+printf("%2.2f is a float..and it's pie!\n",3.1415926535);
+
+return 0;
+}
