aec143a5b56d9f10b74627d077828cd6d45a7b03
Aaron Green <53711550+skyguy6153@users.noreply.github.com>
2021-05-14 17:44:58 -0400

    Add files via upload

---
+++ First Variable!.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+int x;
+int a;
+int y;
+
+x=6;
+a=2;
+y=12;
+printf("The value of variable x is %d.\n",x);
+printf("The value of variable a is %d.\n",a);
+printf("%d is going to be an even number.\n",y);
+return 0;
+}
