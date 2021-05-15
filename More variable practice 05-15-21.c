041ae25ec39f546d98b6ea4c8d13e37a384c2be5
Aaron Green <53711550+skyguy6153@users.noreply.github.com>
2021-05-15 09:53:32 -0400

    Add files via upload

---
+++ More variable practice 05-15-21.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+char c;
+int i;
+float f;
+double d;
+
+c='b';
+i=286;
+f=16.93;
+d=23681.849;
+
+printf("%c\n",c);
+printf("%d\n",i);
+printf("%2.2f\n",f);
+printf("%5.3f\n",d);
+return 0;
+}
