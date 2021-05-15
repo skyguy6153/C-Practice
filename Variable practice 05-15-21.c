2a2c214cad9ea68e7936131e07fc577b39fc8e3b
Aaron Green <53711550+skyguy6153@users.noreply.github.com>
2021-05-15 09:39:24 -0400

    Add files via upload

---
+++ Variable practice 05-15-21.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+char c;
+//First step is to declare the type of variable it is, and that variable's name.
+c='r';
+//Next assign a value to that variable. I chose r.
+
+printf("%c\n",'r');
+return 0;
+}
